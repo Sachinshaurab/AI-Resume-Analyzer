36ce3000a1c789440fa067dc638fa8ba4bab45f0
Sachinshaurab <Sachinarya7887@gmail.com>
2026-07-31 19:36:35 +0530

    Fix CORS

--- server/main.py
+++ server/main.py
@@ -9,7 +9,7 @@ gemini_client=genai.Client()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "https://ai-resume-analyzer-xi-indol.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
